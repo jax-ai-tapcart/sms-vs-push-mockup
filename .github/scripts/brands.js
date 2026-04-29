d8e28e1a9a32c6c26b68893f732b1dec1b30c6c3
Jax <jax@tapcart.co>
2026-04-28 20:22:05 -0700

    feat: brand data for SMS vs Push

---
+++ .github/scripts/brands.js
@@ -0,0 +1,17 @@
+export const BRANDS = {
+  lola: {
+    name: "Lola Blankets",
+    url: "lolablankets.com",
+    logo: "https://graph.facebook.com/lolablankets/picture?type=large",
+    logoBg: "#C4A882",
+    hero: "https://cdn.shopify.com/s/files/1/0538/5931/9979/collections/IMG_4913_1512x_c8bc4628-5d0c-410b-8c90-2440b238d161.jpg?v=1668790452",
+    sms1Text: "Lola Blankets: We\u2019re glad you joined! Stay tuned for more news, & if you have any questions, email us at help@lolablankets.com. Msg frequency varies. Reply STOP to end.",
+    sms1EmailLink: "help@lolablankets.com",
+    sms2Line1: "Lola Blankets: Kori, your coziest weekend starts now. \u1F6BB Take 20% off sitewide with code LOLA20 \u2014 but only for the next 24 hours.",
+    sms2Link: "https://lolablankets.com/CyuPTzr47ul",
+    sms2Line3: "Reply STOP to end.",
+    pushTitle: "Kori, your weekend pick.",
+    pushBody: "The softest blanket. Grab yours now.",
+    outName: "SMS vs Push for Lola Blankets"
+  }
+};
